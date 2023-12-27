f245b46247cc2c67a05243f556b3791813210c67
witerth <1186090092@qq.com>
2023-12-27 17:12:05 +0800

    feat: mysql存储历史记录，获取历史记录接口

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.21.0
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-sql-driver/mysql v1.7.1
 	github.com/gorilla/websocket v1.5.1
-	github.com/streadway/amqp v1.1.0
 )
 
 require (
--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/websocket"
 )
+
+var db *sql.DB
+
+type Message struct {
+	Message  string `json:"message"`
+	Time     string `json:"time"`
+	UserInfo string `json:"userInfo"`
+}
 
 type UserInfo struct {
 	UserId   string `json:"userId"`
@@ -30,7 +41,26 @@ var upgrader = websocket.Upgrader{
 
 var sockets sync.Map
 
+func initDB() {
+	var err error
+	db, err = sql.Open("mysql", "witerth:witerth@tcp(localhost:3306)/database?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping the database: %v", err)
+	}
+}
+
 func main() {
+	// 初始化数据库连接
+	initDB()
+
+	// 启动定时任务，每天清理一次
+	go startCleanupJob()
+
 	r := gin.Default()
 
 	// 示例路由
@@ -38,6 +68,92 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	// POST 路由 - 获取消息
+	r.POST("/history", func(c *gin.Context) {
+		var requestData struct {
+			Time   string `json:"time"`
+			UserId string `json:"userId"`
+		}
+
+		if err := c.ShouldBindJSON(&requestData); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		// 解析时间戳
+		// timeStamp, err := time.Parse(time.RFC3339, requestData.Time)
+		// if err != nil {
+		// 	c.JSON(400, gin.H{"error": "Invalid timestamp format"})
+		// 	return
+		// }
+
+		var rows *sql.Rows
+
+		// 如果未提供时间参数，则默认获取最后 50 条数据
+		if requestData.Time == "" {
+			rows, err := db.Query("SELECT message, time, user_info FROM messages ORDER BY time DESC LIMIT 10")
+			if err != nil {
+				c.JSON(500, gin.H{"error": err.Error()})
+				return
+			}
+			defer rows.Close()
+
+			var messages []SendData
+			for rows.Next() {
+				var msg SendData
+				var userInfoStr string
+				err := rows.Scan(&msg.Message, &msg.Time, &userInfoStr)
+				if err != nil {
+					c.JSON(500, gin.H{"error": err.Error()})
+					return
+				}
+
+				// 解码 user_info 字段到 UserInfo 结构体
+				err = json.Unmarshal([]byte(userInfoStr), &msg.UserInfo)
+				if err != nil {
+					c.JSON(500, gin.H{"error": "Failed to unmarshal UserInfo"})
+					return
+				}
+
+				messages = append(messages, msg)
+			}
+
+			c.JSON(200, messages)
+			return
+		}
+
+		// 获取该时间和对应 userId 前的 50 条数据
+		rows, err := db.Query("SELECT message, time, user_info FROM messages WHERE time < ? AND user_id = ? LIMIT 10",
+			requestData.Time, requestData.UserId)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		defer rows.Close()
+
+		var messages []SendData
+		for rows.Next() {
+			var msg SendData
+			var userInfoStr string
+			err := rows.Scan(&msg.Message, &msg.Time, &userInfoStr)
+			if err != nil {
+				c.JSON(500, gin.H{"error": err.Error()})
+				return
+			}
+
+			// 解码 user_info 字段到 UserInfo 结构体
+			err = json.Unmarshal([]byte(userInfoStr), &msg.UserInfo)
+			if err != nil {
+				c.JSON(500, gin.H{"error": "Failed to unmarshal UserInfo"})
+				return
+			}
+
+			messages = append(messages, msg)
+		}
+
+		c.JSON(200, messages)
 	})
 
 	// POST 路由
@@ -52,6 +168,21 @@ func main() {
 		messageJSON, err := json.Marshal(sendData)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed"})
+			return
+		}
+
+		// 将 UserInfo 转换为字符串
+		userInfoStr, err := json.Marshal(sendData.UserInfo)
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Failed to marshal UserInfo"})
+			return
+		}
+
+		// 将消息保存到数据库
+		_, err = db.Exec("INSERT INTO messages (message, time, user_id, user_info) VALUES (?, ?, ?, ?)",
+			sendData.Message, sendData.Time, sendData.UserInfo.UserId, string(userInfoStr))
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
 
@@ -85,4 +216,22 @@ func main() {
 
 	// 启动 Gin 服务
 	r.Run(":6565")
+}
+
+func startCleanupJob() {
+	for {
+		// 每隔一天执行一次清理任务
+		time.Sleep(24 * time.Hour)
+
+		// 计算 24 小时前的时间
+		thresholdTime := time.Now().Add(-24 * time.Hour)
+
+		// 执行数据库清理操作
+		_, err := db.Exec("DELETE FROM messages WHERE time < ?", thresholdTime)
+		if err != nil {
+			log.Printf("Failed to clean up messages: %v\n", err)
+		} else {
+			log.Println("Messages older than 24 hours have been cleaned up.")
+		}
+	}
 }
